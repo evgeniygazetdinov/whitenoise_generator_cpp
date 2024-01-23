c1e0b59cce8e9acf93f04916190624c84a7d35df
evgeniygazetdinov <zplacesound@gmail.com>
2024-01-23 09:37:28 +0300

    move to separated files .complites

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"html/template"
+
 	"log"
 	"net/http"
 
@@ -16,107 +16,8 @@ const PORT = ":8081"
 
 var database *sql.DB
 
-type Products struct {
-	Id      int
-	Model   string
-	Company string
-	Price   int
-}
+func InitDb() {
 
-type ViewData struct {
-	Title   string
-	Message string
-}
-
-func DeleteHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	_, err := database.Exec("delete from golang.products where id = ?", id)
-	if err != nil {
-		log.Println(err)
-	}
-
-	http.Redirect(w, r, "/", 301)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-
-	rows, err := database.Query("select * from golang.products")
-	if err != nil {
-		fmt.Println("error")
-	}
-	defer rows.Close()
-	products := []Products{}
-	for rows.Next() {
-		p := Products{}
-		err := rows.Scan(&p.Id, &p.Model, &p.Company, &p.Price)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		products = append(products, p)
-	}
-
-	tmpl, _ := template.ParseFiles("templates/index.html")
-	tmpl.Execute(w, products)
-
-}
-
-func addProduct(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		err := r.ParseForm()
-		if err != nil {
-			log.Println(err)
-		}
-		model := r.FormValue("model")
-		company := r.FormValue("company")
-		price := r.FormValue("price")
-
-		_, err = database.Exec("insert into golang.products(model, company, price) values (?, ?,?)", model, company, price)
-
-		if err != nil {
-			log.Println(err)
-		}
-		http.Redirect(w, r, "/", 301)
-	} else {
-		http.ServeFile(w, r, "templates/create.html")
-	}
-}
-
-func editPage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	prod := Products{}
-	row := database.QueryRow("select * from golang.products where id = ?", id)
-	err := row.Scan(&prod.Id, &prod.Model, &prod.Company, &prod.Price)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, http.StatusText(404), http.StatusNotFound)
-	} else {
-		tmpl, _ := template.ParseFiles("templates/edit.html")
-		tmpl.Execute(w, prod)
-	}
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		err := r.ParseForm()
-		if err != nil {
-			log.Println(err)
-		}
-		id := r.FormValue("id")
-		model := r.FormValue("model")
-		company := r.FormValue("company")
-		price := r.FormValue("price")
-
-		_, err = database.Exec("update golang.products set model = ?, company = ?, price = ? where id = ?", model, company, price, id)
-
-		if err != nil {
-			log.Println(err)
-		}
-		http.Redirect(w, r, "/", 301)
-	}
 }
 
 func main() {
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker-compose -f docker-compose.yml up -d db &&
-go run main.go
+go run main.go handlers.go models.go
