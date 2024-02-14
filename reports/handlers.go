d2725d5e870a800580ac2a27afe30bcdcc376488
evgeniygazetdinov <zplacesound@gmail.com>
2024-02-14 09:38:59 +0300

    working with reports

--- reports/handlers.go
+++ reports/handlers.go
@@ -1,1 +1,12 @@
 package reports
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func ReportsMainHandler(w http.ResponseWriter, r *http.Request) {
+	reports := IndexReportService()
+	tmpl, _ := template.ParseFiles("./reports/templates/index.html")
+	tmpl.Execute(w, reports)
+}
--- reports/models.go
+++ reports/models.go
@@ -1,1 +1,11 @@
 package reports
+
+type Report struct {
+	Id      int
+	Name    string
+	Rows    int
+	Columns int
+	Query   string
+}
+
+// add into base queries that struct
